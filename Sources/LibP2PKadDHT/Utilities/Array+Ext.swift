cc9fce920b7092329e9b40f23fd2a0a5af6c2a96
Brandon Toms <btoms.20@gmail.com>
2022-09-09 10:54:40 -0700

    Misc Array Extensions

---
+++ Sources/LibP2PKadDHT/Utilities/Array+Ext.swift
@@ -0,0 +1,37 @@
+//
+//  Array+Ext.swift
+//  
+//
+//  Created by Brandon Toms on 9/9/22.
+//
+
+import LibP2PCore
+
+extension Array where Element == KadDHT.Node {
+    func sortedAbsolutely(using keyspace:KadDHT.Key.KeySpace = .xor) -> [KadDHT.Node] {
+        let targetKey = KadDHT.Key(preHashedBytes: Array<UInt8>(repeating: 0, count: 32))
+        return self.sorted { lhs, rhs in
+            let comp = targetKey.compareDistancesFromSelf(to: KadDHT.Key(lhs.peerID, keySpace: keyspace), and: KadDHT.Key(rhs.peerID, keySpace: keyspace))
+            return comp > 0
+        }
+    }
+    
+    func sorted(closestTo:KadDHT.Node, using keyspace:KadDHT.Key.KeySpace = .xor) -> [KadDHT.Node] {
+        let targetKey = KadDHT.Key(closestTo.peerID, keySpace: keyspace)
+        return self.sorted { lhs, rhs in
+            let comp = targetKey.compareDistancesFromSelf(to: KadDHT.Key(lhs.peerID, keySpace: keyspace), and: KadDHT.Key(rhs.peerID, keySpace: keyspace))
+            return comp > 0
+        }
+    }
+}
+
+extension Array where Element == UInt8 {
+    func zeroPrefixLength() -> Int {
+        var zeros = 0
+        for byte in self {
+            zeros += byte.leadingZeroBitCount
+            if byte.leadingZeroBitCount != 8 { break }
+        }
+        return zeros
+    }
+}
