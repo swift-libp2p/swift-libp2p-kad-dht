58c42916e7c23d152305f8235da1ee1429deace9
Brandon Toms <btoms.20@gmail.com>
2022-09-15 11:12:38 -0700

    Updated to use the new Response API (currently in libp2p/dev). Started thinking about DHT route definitions.

--- Sources/LibP2PKadDHT/Routes/routes.swift
+++ Sources/LibP2PKadDHT/Routes/routes.swift
@@ -11,10 +11,45 @@ import LibP2P
 func registerDHTRoute(_ app:Application) throws {
     app.group("ipfs", "kad") { kad in
         
-        kad.on("1.0.0", handlers: []) { req -> EventLoopFuture<ResponseType<ByteBuffer>> in
+        kad.on("1.0.0", handlers: []) { req -> EventLoopFuture<Response<ByteBuffer>> in
             
             return req.application.dht.kadDHT.processRequest(req)
             
         }
     }
 }
+
+///// KadDHT Route Endpoint - /ipfs/kad/1.0.0
+//func registerDHTRoute2(_ app:Application) throws {
+//    app.group("ipfs", "kad") { kad in
+//
+//        kad.on("1.0.0", handlers: []) { dht in
+//
+//            dht.namespace("pk", validator: DHT.PubKeyValidator) { pk in
+//                pk.onPutRequest { req in
+//                    return req.application.dht.kadDHT.processRequest(req)
+//                }
+//                pk.onGetRequest { req in
+//                    return req.application.dht.kadDHT.processRequest(req)
+//                }
+//            }
+//
+//            dht.namespace("ipns", validator: DHT.BaseValidator.defaultAllowAll) { ipns in
+//                ipns.onPutRequest { req in
+//                    return req.application.dht.kadDHT.processRequest(req)
+//                }
+//                ipns.onGetRequest { req in
+//                    return req.application.dht.kadDHT.processRequest(req)
+//                }
+//            }
+//
+//            dht.namespace("*", validator: DHT.BaseValidator.defaultAllowAll) { wildcard in
+//
+//                // Log Wildcard Namespace Events if you're curious...
+//
+//
+//            }
+//
+//        }
+//    }
+//}
