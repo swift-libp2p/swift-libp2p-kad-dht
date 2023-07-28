a57fd4950a4ccb138976579f5d1975ebec43c1ea
Brandon <32753167+btoms20@users.noreply.github.com>
2023-07-28 10:38:44 -0700

    Added a global switch for CPL Bits vs Bytes. Removed unused code.

--- Sources/LibP2PKadDHT/Utilities/ID.swift
+++ Sources/LibP2PKadDHT/Utilities/ID.swift
@@ -1,52 +1,35 @@
 //
 //  ID.swift
-//  
+//
 //
 //  Created by Brandon Toms on 4/29/22.
 //
 
 import LibP2P
-import CryptoSwift
 
 typealias ID = [UInt8]
 
 extension ID {
-    func commonPrefixLength(with:ID) -> Int {
-        self.commonPrefixLengthBits(with: with)
+    func commonPrefixLength(with: ID) -> Int {
+        if KadDHT.CPL_BITS_NOT_BYTES {
+            return self.commonPrefixLengthBits(with: with)
+        } else {
+            return self.commonPrefixLengthBytes(with: with)
+        }
     }
-    
-    func commonPrefixLengthBytes(with:ID) -> Int {
+
+    func commonPrefixLengthBytes(with: ID) -> Int {
         self.enumerated().first { elem in
             elem.element != with[elem.offset]
         }?.offset ?? 0
     }
-    
-//    func commonPrefixLengthBits(with:ID) -> Int {
-//        let clpBytes = self.enumerated().first { elem in
-//            elem.element != with[elem.offset]
-//        }?.offset ?? self.count
-//
-//        var bits = clpBytes * 8
-//        if clpBytes < self.count {
-//            let bits1:[Bit] = self[clpBytes].bits()
-//            let bits2:[Bit] = with[clpBytes].bits()
-//
-//            let clpBits = bits1.enumerated().first { elem in
-//                elem.element != bits2[elem.offset]
-//            }?.offset ?? 8
-//            bits += clpBits
-//        }
-//
-//        return bits
-//    }
-    
-    func commonPrefixLengthBits(with key:ID) -> Int {
-        distanceBetween(key: self, and: key).zeroPrefixLength()
+
+    func commonPrefixLengthBits(with key: ID) -> Int {
+        self.distanceBetween(key: self, and: key).zeroPrefixLength()
     }
-    
-    private func distanceBetween(key key1:[UInt8], and key2:[UInt8]) -> [UInt8] {
+
+    private func distanceBetween(key key1: [UInt8], and key2: [UInt8]) -> [UInt8] {
         guard key1.count == key2.count else { print("Error: Keys must be the same length"); return [] }
         return key1.enumerated().map { idx, byte in key2[idx] ^ byte }
     }
 }
-
