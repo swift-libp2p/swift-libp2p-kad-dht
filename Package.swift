b00eafce7fe289051c70e9cb56d21ca519b4fbc9
Brandon <32753167+btoms20@users.noreply.github.com>
2025-03-06 21:05:02 -0800

    formatted

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/swift-libp2p/swift-libp2p.git", .upToNextMinor(from: "0.2.0")),
-        
+
         // Testing dependencies
         .package(url: "https://github.com/swift-libp2p/swift-libp2p-noise.git", .upToNextMinor(from: "0.1.0")),
         .package(url: "https://github.com/swift-libp2p/swift-libp2p-mplex.git", .upToNextMinor(from: "0.1.0")),
