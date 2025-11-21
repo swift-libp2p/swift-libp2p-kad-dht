b18bc8011f72370206f9f24bfb98a15938a1233f
Brandon <32753167+btoms20@users.noreply.github.com>
2025-11-20 21:42:51 -0800

    bumped dependencies

--- Package.swift
+++ Package.swift
@@ -30,12 +30,12 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/swift-libp2p/swift-libp2p.git", .upToNextMinor(from: "0.2.0")),
+        .package(url: "https://github.com/swift-libp2p/swift-libp2p.git", .upToNextMinor(from: "0.3.0")),
 
         // Testing dependencies
-        .package(url: "https://github.com/swift-libp2p/swift-libp2p-noise.git", .upToNextMinor(from: "0.1.0")),
-        .package(url: "https://github.com/swift-libp2p/swift-libp2p-mplex.git", .upToNextMinor(from: "0.1.0")),
-        .package(url: "https://github.com/swift-libp2p/swift-libp2p-crypto.git", .upToNextMinor(from: "0.1.0")),
+        .package(url: "https://github.com/swift-libp2p/swift-libp2p-noise.git", .upToNextMinor(from: "0.2.0")),
+        .package(url: "https://github.com/swift-libp2p/swift-libp2p-mplex.git", .upToNextMinor(from: "0.2.0")),
+        .package(url: "https://github.com/swift-libp2p/swift-libp2p-crypto.git", .upToNextMinor(from: "0.2.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -46,8 +46,8 @@ let package = Package(
                 .product(name: "LibP2P", package: "swift-libp2p")
             ],
             resources: [
-                .copy("Protobufs/DHT.proto")
-                //.copy("Protobufs/RPC2.proto")
+                .copy("Protobufs/DHT.proto"),
+                .copy("Protobufs/IPNSRecord.proto"),
             ]
         ),
         .testTarget(
